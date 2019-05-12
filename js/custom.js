baa7592928b2890dc51550669fda6650e1a3bc65
SerhiiFilipchuk <47062741+SerhiiFilipchuk@users.noreply.github.com>
2019-05-12 14:33:34 +0300

    responsive

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+// SCROLL TO SECTION //
+
+/* $(document).ready(function () {
+    $('#down').click (function () {
+    $('html, body').animate({
+       scrollTop: $("#scroll-down-to").offset().top
+       }, 1000);
+    });
+   });
+*/
